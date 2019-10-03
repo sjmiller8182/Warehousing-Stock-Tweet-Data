95b9f75e11ec6dea1f7a8b1eb8be7fecbc2a748d
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2019-10-03 00:10:03 -0500

    move scraping utils into folder Start shell install file

---
+++ scrape_utils/python/install.sh
@@ -0,0 +1,20 @@
+# Used for installing all dependencies
+# Call with sudo
+
+# install python requirements from pip
+pip install -r requirements.txt
+
+# install chrome
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb [arch=amd64]  http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+
+# get chromedriver
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+
+# move chromedriver to install path
+mv chromedriver /usr/bin/chromedriver
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
