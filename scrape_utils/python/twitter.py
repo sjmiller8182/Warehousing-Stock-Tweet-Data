abe28d05f1478d40515d949ed27cef6059f128b1
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2019-10-07 23:46:20 -0500

    Add new twitter timeline scraper Add twitter handles

--- scrape_utils/python/twitter.py
+++ scrape_utils/python/twitter.py
@@ -1,13 +1,16 @@
 """
-Basic scraping utility for Twitter based on Twython
+Basic scraping utility for Twitter
 """
 
 
 # imports
 import os
 import time
+import json
+import csv
+from datetime import datetime
 from typing import List, Dict
-from twython import Twython
+import tweepy
 
 
 class TwitterScraper:
@@ -15,15 +18,14 @@ class TwitterScraper:
     Basic scraping utility for Twitter based on Twython
     """
 
-    def __init__(self, path: str, result_type: str = 'recent') -> None:
+    def __init__(self, path: str) -> None:
         """
         Constructor
         result_type: twitter request type (mixed|recent|popular)
         """
-        self.tw_connection = None
-        self.result_type = result_type
-        self.twitter_keys = None
-        self.key_path = path
+        self.connection = None
+        self.keys = None
+        self.path = path
 
     @staticmethod
     def _get_api_keys(path: str) -> List:
@@ -58,15 +60,86 @@ class TwitterScraper:
         Creates a twitter obj and authenticates with Twitter
         path: a path to twitter access file
         """
-        if self.twitter_keys is None:
-            self.twitter_keys = self._get_api_keys(self.key_path)
+        if self.keys is None:
+            self.keys = self._get_api_keys(self.path)
         # be sure to generate from list
-        self.tw_connection = Twython(*self.twitter_keys)        
+        auth = tweepy.OAuthHandler(*self.keys[:2])
+        auth.set_access_token(*self.keys[2:])    
+        self.connection = tweepy.API(auth)   
 
-    def search(self, query_term: str) -> Dict:
+    def get_user_timeline(self, screen_name, items = None, tweet_mode="extended"):
         """
-        Conducts a standard search
+        Use the twitter API to get a user timeline
+        """
+        statuses = list()
+        for status in tweepy.Cursor(self.connection.user_timeline, screen_name=screen_name, tweet_mode=tweet_mode).items():
+            statuses.append(status)
+        return statuses
 
-        https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
-        """
-        return self.tw_connection.search(q=query_term, result_type=self.result_type)
+def string_to_datetime(date_str: str):
+    """
+    Turns a string including date and time like this - Sun Jul 01 21:06:07 +0000 2018 - to a Python datetime object
+    like this - datetime.datetime(2018, 7, 1, 21, 6, 7, tzinfo=datetime.timezone.utc)
+    """
+    return datetime.strptime(date_str, '%a %b %d %H:%M:%S %z %Y')
+
+def get_mentions(content) -> str:
+    mentions = list()
+    for name in content['entities']['user_mentions']:
+        mentions.append(name['screen_name'])
+    return '|'.join(mentions)
+
+def get_urls(content) -> str:
+    mentions = list()
+    for name in content['entities']['urls']:
+        mentions.append(name['expanded_url'])
+    return '|'.join(mentions)
+
+def get_hashtags(content) -> str:
+    mentions = list()
+    for name in content['entities']['hashtags']:
+        mentions.append(name['text'])
+    return '|'.join(mentions)
+
+def get_items(content) -> List[str]:
+    """
+    Assume status._json is passed to content
+    """
+    tweet_id = content['id']
+    text = content['full_text'].replace('\n','').replace('\t','')
+    hashtags = get_hashtags(content)
+    mentions = get_mentions(content)
+    urls = get_urls(content)
+    created_at = str(string_to_datetime(content['created_at']))
+    user_id = content['user']['id']
+    screen_name = content['user']['screen_name']
+    return [tweet_id, text, hashtags, mentions,
+           urls, created_at, user_id, screen_name]
+
+def write_json(tweets) -> None:
+    
+    features = list()
+    
+    for item in tweets:
+        features = get_items(item._json)
+        file_name = features[7] + '_' + features[5].replace(' ','_') + '.txt'
+        with open(file_name, 'w') as outfile:
+            json.dump(item._json, outfile, indent=2)
+
+def write_to_csv(tweets, filename: str) -> None:
+    # the headers are the fields that we identified in step 4
+    headers = ['tweet_id', 'text', 'hashtags', 'mentions', 'urls', 'created_at', 'user_id', 'screen_name']
+    
+    # here we create the file and write the header row with the headers list
+    # note that the 'filename' argument will be the name of the csv file
+    with open(filename + '.tsv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter='\t')
+        writer.writerow(headers)
+        
+        # in this loop, we write a new row for each tweet object, with the data taken from the tweet object in 
+        # the order we listed the headers
+        # note where we call the helper functions from step 4 on hashtags, urls, and source
+        for item in tweets:
+            writer.writerow(get_items(item._json))
+    csvfile.close()
+
