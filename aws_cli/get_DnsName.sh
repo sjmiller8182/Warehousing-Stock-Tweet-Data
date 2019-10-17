e4d47aaaa69ac26126d3236b6d445c5f128cd023
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2019-10-16 22:00:53 -0500

    Fix master public nds name grab

--- aws_cli/get_DnsName.sh
+++ aws_cli/get_DnsName.sh
@@ -3,6 +3,11 @@
 # CLI command for getting the DNS name of the EMR cluster
 #
 
+# just get the stored name of the EMR cluster
 emr_cluster_name=$(head -n 1 cluster_name.txt)
 
-aws emr list-instances --cluster-id $emr_cluster_name --region us-east-2
+# get Public DNS name of master node from instance list
+aws emr describe-cluster \
+        --cluster-id $emr_cluster_name \
+        --region us-east-2 | \
+        jq '.Cluster.MasterPublicDnsName'
