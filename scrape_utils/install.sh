c1c6247df5d630f026550b322dc36daa4ec11155
Stuart M <stuart.j.miller.8182@gmail.com>
2019-10-31 22:27:30 -0500

    Update install.sh
    Add anaconda install
    Add R requirements install

--- scrape_utils/install.sh
+++ scrape_utils/install.sh
@@ -3,8 +3,17 @@
 # Used for installing all dependencies
 # Call with sudo
 
+# install anaconda
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+# might need to pipe 'yes' here...
+bash Anaconda3-2019.03-Linux-x86_64.sh
+# check if there is some user interaction needed...
+
 # install python requirements from pip
 pip install -r requirements.txt
+
+# install R requirements
+Rscript -e ./requirements_R.R
 
 # install chrome
 sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
