c0bcbd96345e681da128f58e0178d441643eb0a0
Stuart M <stuart.j.miller.8182@gmail.com>
2019-10-31 22:18:24 -0500

    Create requirements_R.R

---
+++ scrape_utils/requirements_R.R
@@ -0,0 +1,27 @@
+#!/usr/bin/R
+
+# just install the packages required for the R scripts
+
+install.packages(c("tidyverse",
+                   "pacman",
+                   "xts",
+                   "alphavantager",
+                   "knitr",
+                   "testthat",
+                   "stringr",
+                   "stringi",
+                   "sparklyr",
+                   "aws.s3",
+                   "textreadr", 
+                   "NLP", 
+                   "tidytext", 
+                   "wordcloud", 
+                   "DT", 
+                   "ggthemes", 
+                   "kableExtra", 
+                   "hunspell",
+                   "stringdist", "
+                   qdap", 
+                   "rJava", 
+                   "slam", 
+                   "RCurl"))
