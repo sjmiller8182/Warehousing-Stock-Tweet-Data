3b4235a7363b3c4cdf1e0e9a5354ebd18751e295
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2019-11-30 20:42:28 -0600

    Add diag plots for anova

--- results_analysis/Two_Way_ANOVA_with_Repeateds.R
+++ results_analysis/Two_Way_ANOVA_with_Repeateds.R
@@ -4,3 +4,9 @@ library(lmerTest)
 fit <- lmer(time ~ schema + block_size + schema*block_size  + (1|block_size), data=df)
 anova(fit)
 summary(fit)
+
+residuals <- resid(fit)
+pred <- predict(fit, df)
+
+hist(residuals)
+plot(pred, residuals)
